ee1054cdc7bd1484e625213b2b359d0a513e29e8
Subodh Nijsure <snijsure@mocana.com>
2016-01-22 12:41:56 -0800

    - Fixed array out-of-bound issue in nextStep. - Also in nextStep cache the neighbor count before actually setting the state. - Implemented getNeighors method that makes code bit easy to  understand. - Also Added few changes as suggested by Android Lint.

--- app/src/main/java/com/goprimer/gameoflife/Cell.java
+++ app/src/main/java/com/goprimer/gameoflife/Cell.java
@@ -3,56 +3,96 @@ package com.goprimer.gameoflife;
 import android.content.Context;
 import android.graphics.Color;
 import android.widget.Button;
-
 public class Cell extends Button {
 
     private static final int ALIVE = Color.BLUE;
     private static final int DEAD = Color.GRAY;
-
+    private static final String TAG = "GOL/Cell";
     static final int X_SIZE = 16;
     static final int Y_SIZE = 16;
+    Element mElements[];
+
+
+    // TODO:
+    // From OO perspective Cell class should create array of
+    // Elements, shown below and move all the static methods
+    // from Cell object to operational methods on object Element.
+
+    class Element {
+        private int status = DEAD;
+        private int neighbors = 0;
+        public int  getState() { return status; }
+        public void setState(int i) { status = i; }
+        public void setNeighbor(int c) { neighbors = c; }
+        public int  getNeighbors() { return neighbors; }
+    };
 
     private int status;
+    private int neighbors;
 
     public Cell(Context context) {
         super(context);
-
+        mElements = new Element[X_SIZE * Y_SIZE];
         this.setStatus(DEAD);
     }
 
-    public static int countLiveNeighbors(Cell[][] cells, int x, int y) {
-        int liveNeighbors = 0;
-
-        for (int dx = -1; dx <= 1; dx++) {
-            for (int dy = -1; dy <= 1; dy++) {
-                int neighborX = x + dx;
-                int neighborY = y + dy;
-                if (cells[neighborX][neighborY].status == ALIVE) {
-                    liveNeighbors++;
-                }
-            }
+    private static int getState(Cell[][] cells, int row, int col) {
+        try {
+            if (cells[row][col].status == ALIVE)
+                return 1;
+            else
+                return 0;
         }
+        catch( ArrayIndexOutOfBoundsException outOfBoundException) {
+            // Indicates that there is no such neighbour
+            return 0;
+        }
+    }
+    private static int getNeighbors(Cell[][] cells,int row, int col) {
+        int count = 0;
+        count += Cell.getState(cells,row-1, col-1);
+        count += Cell.getState(cells,row-1, col);
+        count += Cell.getState(cells,row-1, col+1);
 
-        return liveNeighbors;
+        count += Cell.getState(cells,row, col-1);
+        count += Cell.getState(cells,row, col+1);
+
+        count += Cell.getState(cells,row+1, col-1);
+        count += Cell.getState(cells,row+1, col);
+        count += Cell.getState(cells,row+1, col+1);
+        return count;
     }
 
     public static void nextStep(Cell[][] cells) {
-        for (int x = 0; x <= X_SIZE; x++) {
-            for (int y = 0; y <= Y_SIZE; y++) {
-                int liveNeighbors = Cell.countLiveNeighbors(cells, x, y);
-                if (liveNeighbors = 3) {
+
+        // Cache neighbors before we go about setting the state.
+        for (int x = 0; x < X_SIZE; x++) {
+            for (int y = 0; y < Y_SIZE; y++) {
+                cells[x][y].neighbors = Cell.getNeighbors(cells, x, y);
+            }
+        }
+        for (int x = 0; x < X_SIZE; x++) {
+            for (int y = 0; y < Y_SIZE; y++) {
+                int liveNeighbors = cells[x][y].neighbors;
+                if (liveNeighbors == 3) {
                     cells[x][y].setStatus(ALIVE);
                 } else if (liveNeighbors != 2) {
-                    cells[x][y].setStatus(DEAD);
+                    try {
+                        cells[x][y].setStatus(DEAD);
+                    }
+                    catch( ArrayIndexOutOfBoundsException outOfBoundException) {
+                        // Indicates that there is no such neighbour
+                    }
                 }
             }
         }
     }
 
-    public static void clearBoard(Cell[][] cells) {
+    private static void clearBoard(Cell[][] cells) {
         for (int x = 0; x < X_SIZE; x++) {
             for (int y = 0; y < Y_SIZE; y++) {
                 cells[x][y].setStatus(DEAD);
+                cells[x][y].neighbors = 0;
             }
         }
     }
